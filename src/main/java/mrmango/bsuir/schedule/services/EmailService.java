be0d74052edf3ff2498c9f26eef0558f0911a1ff
Ilya <ilyanerd@gmail.com>
2018-06-03 14:29:00 +0300

    imports

--- src/main/java/mrmango/bsuir/schedule/services/EmailService.java
+++ src/main/java/mrmango/bsuir/schedule/services/EmailService.java
@@ -5,10 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
-import javax.mail.internet.MimeMessage;
 import java.util.List;
 
 /**
