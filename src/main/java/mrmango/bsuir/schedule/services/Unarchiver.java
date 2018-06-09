54bd0b47252201610830f80b0e3a22aca0134940
Ilya <ilyanerd@gmail.com>
2018-06-09 19:40:40 +0300

    improved unarchiver

--- src/main/java/mrmango/bsuir/schedule/services/Unarchiver.java
+++ src/main/java/mrmango/bsuir/schedule/services/Unarchiver.java
@@ -3,6 +3,7 @@ package mrmango.bsuir.schedule.services;
 import de.innosystec.unrar.Archive;
 import de.innosystec.unrar.rarfile.FileHeader;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -13,15 +14,21 @@ import java.io.FileOutputStream;
  * on 27-May-2018
  */
 @Service
+@Log4j2
 public class Unarchiver {
 
     @SneakyThrows
-    public void unrar(File file) {
-        File archiveDir = new File(file.getParent() + "/" + file.getName().replace(".rar", ""));
-        archiveDir.mkdir();
-        try (Archive archive = new Archive(file)) {
+    public void unrar(File archiveFile) {
+        log.info("Unraring file [" + archiveFile.getPath() + "]");
+        File archiveDir = new File(
+                archiveFile.getParent() + "/" + archiveFile.getName().replace(".rar", ""));
+        if (!archiveDir.mkdirs()) {
+            log.warn("Unable to mkdirs: " + archiveDir.getPath());
+        }
+        try (Archive archive = new Archive(archiveFile)) {
             for (FileHeader fileHeader : archive.getFileHeaders()) {
-                try (FileOutputStream os = new FileOutputStream(new File(archiveDir, fileHeader.getFileNameString()))) {
+                File unraredFile = new File(archiveDir, fileHeader.getFileNameString());
+                try (FileOutputStream os = new FileOutputStream(unraredFile)) {
                     archive.extractFile(fileHeader, os);
                 }
             }
