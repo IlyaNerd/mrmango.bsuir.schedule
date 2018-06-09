9562fd3b15586a3a02e4de54d71e00f90cefee4d
Ilya <ilyanerd@gmail.com>
2018-06-09 23:13:34 +0300

    added method to moved already parsed files

--- src/main/java/mrmango/bsuir/schedule/services/MarkedFileService.java
+++ src/main/java/mrmango/bsuir/schedule/services/MarkedFileService.java
@@ -1,11 +1,13 @@
 package mrmango.bsuir.schedule.services;
 
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
  * on 09-Jun-2018
  */
 @Service
+@Log4j2
 public class MarkedFileService {
     private final PdfParser pdfParser;
     private final String dir;
@@ -30,10 +33,23 @@ public class MarkedFileService {
 
     @SneakyThrows
     public List<File> getMarkedPdfFiles() {
+        log.info("Searching for marked pdf files in " + dir);
         return Files.list(Paths.get(dir))
                 .filter(path -> path.toString().endsWith(".pdf"))
                 .map(Path::toFile)
                 .filter(pdfParser::pdfContainsAnnotations)
                 .collect(Collectors.toList());
+    }
+
+    @SneakyThrows
+    public void moveMarkedFiles(List<File> markedFiles) {
+        log.info("Moving parsed files to ./parsed dir");
+        try {
+            Files.createDirectory(Paths.get(dir + "/parsed"));
+        } catch (FileAlreadyExistsException ignored) {
+        }
+        for (File file : markedFiles) {
+            Files.move(file.toPath(), Paths.get(dir + "/parsed/" + file.getName()));
+        }
     }
 }
